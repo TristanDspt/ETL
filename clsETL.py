1769482ea1dcdf026f4817f138891778a536c1e0
TristanDspt <tristan.despont@hotmail.fr>
2025-07-18 13:53:17 +0200

    debut fonction lire csv + ajout tools

--- clsETL.py
+++ clsETL.py
@@ -1,1 +1,5 @@
-def lire_csv 
+from tools import ouvrir_fichier_local
+
+def lire_csv(nom_fichier: str, supprimer_lignes_vides = True) -> list[str]:
+    with ouvrir_fichier_local("Personnes.csv", "r", "utf-8") as f:
+        
