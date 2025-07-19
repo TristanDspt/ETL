6a418c0fa1e51ebb102ab4061e190f2bbcb06431
TristanDspt <tristan.despont@hotmail.fr>
2025-07-19 20:49:37 +0200

    ecriture script

--- TestETL.py
+++ TestETL.py
@@ -1,1 +1,8 @@
 import clsETL
+
+demander_suppression_ligne = input("Voulez vous supprimer les lignes vides ? o/n : ").lower().strip()
+suppression = demander_suppression_ligne == "o"
+
+donnee_lue = clsETL.extract(demander_suppression_ligne = suppression)
+donnee_transforme = clsETL.transform(donnee_lue)
+donnee_chargee = clsETL.load(donnee_transforme)
--- clsETL.py
+++ clsETL.py
@@ -1,5 +1,26 @@
-from tools import ouvrir_fichier_local
+import csv
+import json
 
-def lire_csv(nom_fichier: str, supprimer_lignes_vides = True) -> list[str]:
-    with ouvrir_fichier_local("Personnes.csv", "r", "utf-8") as f:
-        
+def extract(filename="e:\\Data\\Tristan\\Document\\Programmation\\Python\\ETL\\Personnes.csv", separateur=";", demander_suppression_ligne = True):
+    data = []
+    try:
+        with open(filename, "r",encoding="utf-8") as fichier:
+                lire_csv = csv.DictReader(fichier, delimiter=separateur)
+                for ligne in lire_csv:
+                    if demander_suppression_ligne:
+                        if any(val.strip() for val in ligne.values()):
+                            data.append(ligne)
+                    else:
+                        data.append(ligne)
+    except FileNotFoundError:
+        print(f"Erreur : fichier {filename} introuvable.")
+    return data
+
+def transform(data_to_transform):
+    # Pas de modification, on renvoie tel quel
+    return data_to_transform
+
+def load(data_to_load):
+    with open ("personnes.json", "w", encoding="utf-8") as fichier:
+        json.dump(data_to_load, fichier, indent = 4, ensure_ascii=False)
+    return data_to_load
