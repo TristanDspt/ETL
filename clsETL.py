4f524fabb3fd7c8b2141f330a559e82a83dc65ca
TristanDspt <tristan.despont@hotmail.fr>
2025-07-21 10:51:36 +0200

    ajout fonction saisie de ficher utilisateur

--- clsETL.py
+++ clsETL.py
@@ -1,10 +1,14 @@
 import csv
 import json
 
-def extract(filename="e:\\Data\\Tristan\\Document\\Programmation\\Python\\ETL\\Personnes.csv", separateur=";", demander_suppression_ligne = True):
+def extract(filename=None, separateur=";", demander_suppression_ligne = True):
     data = []
+
+    if filename is None:
+        filename = input("Chemin du fichier CSV : ").lower().strip()
+
     try:
-        with open(filename, "r",encoding="utf-8") as fichier:
+        with open(filename, "r", encoding="utf-8") as fichier:
                 lire_csv = csv.DictReader(fichier, delimiter=separateur)
                 for ligne in lire_csv:
                     if demander_suppression_ligne:
@@ -21,6 +25,11 @@ def transform(data_to_transform):
     return data_to_transform
 
 def load(data_to_load):
-    with open ("personnes.json", "w", encoding="utf-8") as fichier:
+
+    nom_sortie = input("Nom du fichier de sortie : ").lower().strip()
+    if not nom_sortie.endswith(".json"):
+        nom_sortie += ".json"
+
+    with open (nom_sortie, "w", encoding="utf-8") as fichier:
         json.dump(data_to_load, fichier, indent = 4, ensure_ascii=False)
     return data_to_load
