f6bf3411ec0dde3b6ea586a478e6835e204986bf
TristanDspt <tristan.despont@hotmail.fr>
2025-07-23 20:04:38 +0200

    main fonctionnel

--- TestETL.py
+++ TestETL.py
@@ -4,22 +4,21 @@ chemin = input("Chemin du fichier CSV : ").lower().strip()
 demander_suppression_ligne = input("Voulez vous supprimer les lignes vides ? o/n : ").lower().strip()
 suppression = demander_suppression_ligne == "o"
 
-donnee_lue = clsETL.extract(filename=chemin, demander_suppression_ligne=suppression)
+donnee_lue = clsETL.extract_manager(filename=chemin, demander_suppression_ligne=suppression)
+
 donnee_transforme = clsETL.transform(donnee_lue)
-donnee_chargee = clsETL.load(donnee_transforme, filename=chemin)
 
-def choix_format_extract():
-    print("Choisissez le format d'enregistrement :")
-    print("1 - CSV")
-    print("2 - JSON")
-    print("3 - XML")
-    choix = input("Votre choix : ")
-
-    if choix == "1":
-        clsETL.load_manager(data, filename, "csv")
-    elif choix == "2":
-        clsETL.load_manager(data, filename, "json")
-    elif choix == "3":
-        clsETL.load_manager(data, filename, "xml")
-    else:
+print("Choisissez le format d'enregistrement :")
+print("1 - CSV")
+print("2 - JSON")
+print("3 - XML")
+choix = input("Votre choix : ")
+match choix:
+    case "1":
+        clsETL.load_csv(donnee_transforme, filename=chemin)
+    case "2":
+        clsETL.load_json(donnee_transforme, filename=chemin)
+    case "3":
+        clsETL.load_xml(donnee_transforme, filename=chemin)
+    case _:
         print("Format non supporté.")
