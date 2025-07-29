b1aa1ad52b2183f7b8860f37f10795ad928cec29
TristanDspt <tristan.despont@hotmail.fr>
2025-07-29 13:33:07 +0200

    refactorisation + commentaires

--- TestETL.py
+++ TestETL.py
@@ -1,4 +1,4 @@
-import clsETL
+from clsETL import extract_manager, transform, load_manager
 import os
 
 chemin = input("Chemin du fichier CSV : ").lower().strip()
@@ -9,9 +9,6 @@ if filetype == ".csv":
     if choix_sep in ("", ";"):
         separateur = ";"
     elif choix_sep == "tab":
-        #La gestion du tab est à reporter dans le code de clsETL.py
-        # car c'est une fonctionnalité de l'extract_manager d'autant que si tu te mets à gérer d'autres séparateurs
-        # spéciaux, tu vas devoir modifier le code à chaque fois.
         separateur = "\t"
     elif len(choix_sep) == 1:
         separateur = choix_sep
@@ -23,28 +20,16 @@ demander_suppression_ligne = input("Voulez vous supprimer les lignes vides ? o/n
 
 suppression = demander_suppression_ligne == "o"
 
-donnee_lue = clsETL.extract_manager(filename=chemin, separateur=separateur, demander_suppression_ligne=suppression)
+donnee_lue = extract_manager(filename=chemin, separateur=separateur, demander_suppression_ligne=suppression)
 
 colonnes_a_supprimer = ["rowguid", "AdditionalContactInfo", "Demographics"]
 
-donnee_transforme = clsETL.transform(donnee_lue, remove_col=colonnes_a_supprimer)
+donnee_transforme = transform(donnee_lue, remove_col=colonnes_a_supprimer)
 
-# d'un point de vue formel, le bloc avant le match est à déplacer dans la section ou tu interroge l'utilisateur
-# pour toutes les données que tu lui demande. 
-# cela devrait d'ailleurs être dans une fonction qui retournerai la liste des valeurs saisies
-# comme cela si un jour on décide de mettre en place une vraie interface graphique,
-# on pourra facilement adapater le code
 print("Choisissez le format d'enregistrement :")
 print("1 - CSV")
 print("2 - JSON")
 print("3 - XML")
-choix = input("Votre choix : ")
-match choix:
-    case "1":
-        clsETL.load_csv(donnee_transforme, filename=chemin)
-    case "2":
-        clsETL.load_json(donnee_transforme, filename=chemin)
-    case "3":
-        clsETL.load_xml(donnee_transforme, filename=chemin)
-    case _:
-        print("Format non supporté.")
+choix = input("Votre choix : ").lower().strip()
+
+load_manager(data_to_load=donnee_transforme, filename=chemin, choix=choix)
--- clsETL.py
+++ clsETL.py
@@ -4,32 +4,44 @@ import os
 import xml.etree.ElementTree as ET
 
 def extract_manager(filename, separateur=None, demander_suppression_ligne=True):
-    # ici la valeur par défaut de separateur est None, ce qui n'est pas logique, une valeur ';' 
-    # est plus logique car c'est le séparateur le plus courant
+    """
+    Gestion des méthodes d'extraction.
+    
+    Args:
+        filename (str): Nom du fichier à lire.
+        separateur (str): Caractère séparateur utilisé dans le fichier CSV.
+        demander_suppression_ligne (bool): Supprimer les lignes vides si True..
+        
+    Returns:
+        Méthode associée.
+    """
     filetype = os.path.splitext(filename)[1].lower()
     
     match filetype:
         case ".csv":
-            # dans le manager le code est faux, tu demande toujours la suppression des lignes!
-            # c'est le cas de tous les autres appels. Il suffit de passer le paramètre reçu.
-            return extract_csv(filename, separateur=separateur, demander_suppression_ligne=True)
+            if separateur is None:
+                separateur = ";"
+            return extract_csv(filename, separateur, demander_suppression_ligne)
         case ".json":
-            return extract_json(filename, demander_suppression_ligne=True)
+            return extract_json(filename, demander_suppression_ligne)
         case ".xml":
-            return extract_xml(filename, demander_suppression_ligne=True)
+            return extract_xml(filename, demander_suppression_ligne)
         case _:
             raise ValueError("Type de fichier non supporté")
 
-def extract_csv(filename=None, separateur=";", demander_suppression_ligne=True):
-    # la valeur par défaut de filename est None, ce qui n'est pas logique
-    # car tu ne peux pas ouvrir un fichier qui n'existe pas
-    # a ce niveau aucune valeur par défaut n'est logique car tu est sur un appel secondaire du manager
-    # qui gère déjà les valeurs par défaut
+def extract_csv(filename, separateur, demander_suppression_ligne):
+    """
+    Extrait un fichier CSV dans une liste de dictionnaires.
+    
+    Args:
+        filename (str): Nom du fichier à lire.
+        separateur (str): Caractère séparateur utilisé dans le fichier CSV.
+        demander_suppression_ligne (bool): Supprimer les lignes vides si True.
+
+    Returns:
+        list of dict: Données extraites sous forme de liste de dictionnaires.
+    """
     data = []
-    # les 2 lignes ci-dessus sont inutiles car le séparateur est déjà géré l'appel
-    # par contre tu ne gère pas le cas du tab 
-    if separateur is None:
-        separateur = ";"
     try:
         with open(filename, "r", encoding="utf-8") as fichier:
                 lire_csv = csv.DictReader(fichier, delimiter=separateur)
@@ -43,7 +55,18 @@ def extract_csv(filename=None, separateur=";", demander_suppression_ligne=True):
         print(f"Erreur : fichier {filename} introuvable.")
     return data
 
-def extract_json(filename=None, separateur=None, demander_suppression_ligne=True):
+def extract_json(filename, demander_suppression_ligne):
+    """
+    Extrait un fichier JSON dans une liste de dictionnaires.
+    
+    Args:
+        filename (str): Nom du fichier à lire.
+        separateur (str): Caractère séparateur utilisé dans le fichier JSON.
+        demander_suppression_ligne (bool): Supprimer les lignes vides si True.
+
+    Returns:
+        list of dict: Données extraites sous forme de liste de dictionnaires.
+    """
     data = []
     try:
         with open(filename, "r", encoding="utf-8") as fichier:
@@ -60,7 +83,18 @@ def extract_json(filename=None, separateur=None, demander_suppression_ligne=True
         print(f"Erreur : fichier {filename} introuvable.")
     return data
 
-def extract_xml(filename=None, separateur=None, demander_suppression_ligne=True):
+def extract_xml(filename, demander_suppression_ligne):
+    """
+    Extrait un fichier XML dans une liste de dictionnaires.
+    
+    Args:
+        filename (str): Nom du fichier à lire.
+        separateur (str): Caractère séparateur utilisé dans le fichier XML.
+        demander_suppression_ligne (bool): Supprimer les lignes vides si True.
+
+    Returns:
+        list of dict: Données extraites sous forme de liste de dictionnaires.
+    """
     data = []
     # à voir plus tard
     return data
@@ -69,70 +103,132 @@ def transform(data_to_transform, remove_col=None):
     if remove_col is None:
         remove_col = []
     for row in data_to_transform:
-        for col in remove_col:
-            row.pop(col, None)
-        # C'est louable de tester la présnce de la colonne
-        # mais ce n'est utile, à ce stade tu dois faire confiance aux données
-        # que tu as lues
-        # si tu veux vraiment tester la présence de la colonne, il faut le faire avant
         if "EmailPromotion" not in row:
             print("Colone absente")
         elif row["EmailPromotion"] == "0":
             row["EmailPromotion"] = "Non" 
-        elif row["EmailPromotion"] == "1" or "2": # ce code est incorrect, il faut vérifier la logique
-                                                  # un in à la place de == 1 or 2 est plus adapté
-                                                  # ou bien si tu veux utiliser un or il fait le faire comme ça
-                                                  # row["EmailPromotion"] == "1" or row["EmailPromotion"] == "2":
-                                                  # On peut discuter ensemble du cas difficile des OR, la logique 
-                                                  # informatique est bien plus rigoureuse que la logique humaine
-                                                  
+        else:                                     
             row["EmailPromotion"] = "Oui"
     return data_to_transform
 
-# l'absence de load_manager est un peu choquante car cela alourdit le code appelant qui doit gérer 
-# le chargement de chaque type de fichier séparément
-# les avantages de l'extract_manager sont tout aussi présents pour le load_manager
-# si tu as l'un, tu as l'autre la cohérence du code est importante
-# la meilleure preuve est que tu as reporté le code de chargement dans le fichier TestETL.py
-def load_json(data_to_load, filename):
-    nom_entree = os.path.splitext(filename)[0].lower()
-    nom_sortie = nom_entree + ".json"
-    if not data_to_load:
-        print("Aucune donnée à écrire.")
+def load_manager(data_to_load, filename, choix):
+    """
+    Gestion des méthodes d'ecriture.
+    
+    Args:
+        data_to_load (dict): Liste de dictionnaires à ecrire.
+        filename (str): Nom du fichier.
+        choix (str): Choix du format de sortie.
+        
+    Returns:
+        Méthode associée.
+    """
+    formats = {
+    "1": "csv",
+    "2": "json",
+    "3": "xml"
+    }
+    format_sortie = formats.get(choix)
+    if not format_sortie:
+        print("Choix invalide.")
         return
-    with open (nom_sortie, "w", encoding="utf-8") as fichier:
-        json.dump(data_to_load, fichier, indent=4, ensure_ascii=False)
-    # A quoi sert la ligne suivante ?
-    # elle n'est pas nécessaire car tu as déjà écrit les données dans le fichier
-    # il serait plus logique de retourner le nom du fichier écrit ou un booléen indiquant que le traitement
-    # s'est bien déroulé. A condition bien sur que tu retourne 0 si le fichier n'a pas été écrit.
-    return data_to_load
+
+    valeur_retour = None
+    match format_sortie:
+        case "csv":
+            valeur_retour = load_csv(data_to_load, filename)
+        case "json":
+            valeur_retour = load_json(data_to_load, filename)
+        case "xml":
+            valeur_retour = load_xml(data_to_load, filename)
+        case _:
+            print("Format non supporté.")
+            return
+
+    if valeur_retour == False:
+        print("Aucune donnée à écrire.")
+    else:
+        print(f"Fichier créé : {filename}")
+
+def generer_nom_sortie(filename, extension):
+    """
+    Genere un nom de fichier en sortie.
+    
+    Args:
+        filename (str): Nom du fichier.
+        extension (str): Nom de l'extension.
+        
+    Returns:
+        nom du fichier + extension.
+    """
+    return os.path.splitext(filename)[0].lower() + extension
 
 def load_csv(data_to_load, filename):
-    nom_entree = os.path.splitext(filename)[0].lower()
-    nom_sortie = nom_entree + ".csv"
-    if not data_to_load:
-        print("Aucune donnée à écrire.")
-        return
+    """
+    Ecrit une liste de dictionnaire dans un fichier CSV.
+    
+    Args:
+        data_to_load (dict): Liste de dictionnaires à ecrire.
+        filename (str): Nom du fichier.
+        
+    Returns:
+        bool: True si l’écriture a réussi, False sinon.
+    """
+    nom_sortie = generer_nom_sortie(filename, ".csv")
+
+    if not data_to_load:        
+        return False
+    
     with open (nom_sortie, "w", encoding="utf-8", newline="") as fichier:
         writer = csv.DictWriter(fichier, delimiter=",", fieldnames=data_to_load[0].keys())
         writer.writeheader()
         for row in data_to_load:
             writer.writerow(row)
-    return data_to_load
+    return True
+
+def load_json(data_to_load, filename):
+    """
+    Ecrit une liste de dictionnaire dans un fichier JSON.
+    
+    Args:
+        data_to_load (dict): Liste de dictionnaires à ecrire.
+        filename (str): Lom du fichier.
+        
+    Returns:
+        bool: True si l’écriture a réussi, False sinon.
+    """
+    nom_sortie = generer_nom_sortie(filename, ".json")
+
+    if not data_to_load:
+        return False
+    
+    with open (nom_sortie, "w", encoding="utf-8") as fichier:
+        json.dump(data_to_load, fichier, indent=4, ensure_ascii=False)
+    return True
 
 def load_xml(data_to_load, filename):
-    nom_entree = os.path.splitext(filename)[0].lower()
-    nom_sortie = nom_entree + ".xml"
+    """
+    Ecrit une liste de dictionnaire dans un fichier XML.
+    
+    Args:
+        data_to_load (dict): Liste de dictionnaires à ecrire.
+        filename (str): Nom du fichier.
+        
+    Returns:
+        bool: True si l’écriture a réussi, False sinon.
+    """
+    nom_sortie = generer_nom_sortie(filename, ".xml")
+
     if not data_to_load:
-        print("Aucune donnée à écrire.")
-        return
+        return False
+    
     root = ET.Element("données")
     for row in data_to_load:
         item = ET.SubElement(root, "ligne")
         for cle, valeur in row.items():
             champ = ET.SubElement(item, cle)
-            champ.text = valeur
+            champ.text = str(valeur)
 
     arbre = ET.ElementTree(root)
     arbre.write(nom_sortie, encoding="utf-8", xml_declaration=True)
+    return True
